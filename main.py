57c98008a5a8e97fd061cd1dec5fc389113f839c
akcaesar <94551947+akcaesar@users.noreply.github.com>
2025-03-16 11:25:48 +0000

    dt clean

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ API Routes for getting stats of the emails fetched from the user's Gmail inbox.
 """
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from typing import List
 from pydantic import BaseModel
-from components.pandas_df import email_df
+from EmailSense_cp.components.email_df import email_df
 import utilities
 
 app = FastAPI()
@@ -19,9 +20,20 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/emails")
+async def get_emails():
+    try:
+        return JSONResponse(content=email_df.to_json(orient="table"), status_code=200)
+    except:
+        return JSONResponse(content={"message": "Error in fetching emails"}, status_code=500)
+
 
 @app.get("/emails/count")
 async def get_emails_count():
-    len = utilities.get_emails_count(email_df)
-    return len
+    try:
+        return JSONResponse(content={"count": utilities.get_emails_count(email_df)}, status_code=200)
+    except:
+        return JSONResponse(content={"message": "Error in fetching emails count"}, status_code=500)
+
+
     
