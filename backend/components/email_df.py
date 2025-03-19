716d922525bd8b521885c4afd2f6a61ffab4bba6
akcaesar <94551947+akcaesar@users.noreply.github.com>
2025-03-19 10:04:56 +0000

    commented out some code temporarily

--- backend/components/email_df.py
+++ backend/components/email_df.py
@@ -12,16 +12,16 @@ emails = fetch_emails()
 email_df = pd.DataFrame(emails)
 email_df_mod = email_df.copy()
 
-# Convert the 'Date' column to datetime
-email_df_mod['Date'] = pd.to_datetime(email_df_mod['Date'], format='%a, %d %b %Y %H:%M:%S %z')
+# # Convert the 'Date' column to datetime
+# email_df_mod['Date'] = pd.to_datetime(email_df_mod['Date'], format='%a, %d %b %Y %H:%M:%S %z')
 
-# Extract date, time, and day
-email_df_mod['date'] = email_df_mod['Date'].dt.date
-email_df_mod['time'] = email_df_mod['Date'].dt.time
-email_df_mod['day'] = email_df_mod['Date'].dt.day_name()
+# # Extract date, time, and day
+# email_df_mod['date'] = email_df_mod['Date'].dt.date
+# email_df_mod['time'] = email_df_mod['Date'].dt.time
+# email_df_mod['day'] = email_df_mod['Date'].dt.day_name()
 
-# Drop the original 'Date' column if no longer needed
-email_df_mod = email_df_mod.drop(columns=['Date'])
+# # Drop the original 'Date' column if no longer needed
+# email_df_mod = email_df_mod.drop(columns=['Date'])
 
 
 
