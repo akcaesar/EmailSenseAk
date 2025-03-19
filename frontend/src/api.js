204eb6604d9cdb07ff6b80da514b28843e1a68e2
akcaesar <94551947+akcaesar@users.noreply.github.com>
2025-03-19 12:28:43 +0000

    corrected baseurl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios, { Axios } from 'axios';
 const token = 'ghu_hrjQKJ62M1zVHnyu5yuHfZ4i00K5Ub2sAWfP';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
+  baseURL: 'https://emailsenseak.onrender.com',
   timeout: 1000,
   headers: { 
     'Authorization': `Bearer ${token}`,
